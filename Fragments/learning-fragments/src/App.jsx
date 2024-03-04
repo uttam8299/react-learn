302bd77165f1963ac04712adb535750f71fc6536
uttam8299 <uttam.yadav8299@gmail.com>
2024-03-05 00:45:08 +0530

    conditional rendering - logical operator

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -6,15 +6,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
 function App() {
-  // let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
 
-  let foodItems = [];
+  // let foodItems = [];
 
   return (
     <>
       <h1>Healthy Food</h1>
-      {foodItems.length == 0 ? <h1>I am still hungry...</h1> : null}
-      {/* null - here value can be any falsy value, falsy value --> null, undefined, empty, NaN, zero */}
+      {foodItems.length == 0 && <h1>I am still hungry...</h1>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
