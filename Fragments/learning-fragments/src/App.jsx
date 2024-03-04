72be935c0a403ddec798863b5092d002d1a4850e
uttam8299 <uttam.yadav8299@gmail.com>
2024-03-05 00:40:34 +0530

    conditional rendering - if/else

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -6,7 +6,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
 function App() {
-  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  // let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+
+  let foodItems = [];
+
+  if (foodItems.length == 0) {
+    return <h1>I'm still hungry...</h1>;
+  }
 
   return (
     <>
