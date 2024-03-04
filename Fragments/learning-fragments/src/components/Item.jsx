4e09ff26fbcd7deaf3e5aa8e27f044e43cc9f40f
uttam8299 <uttam.yadav8299@gmail.com>
2024-03-05 01:37:19 +0530

    css modules

--- Fragments/learning-fragments/src/components/Item.jsx
+++ Fragments/learning-fragments/src/components/Item.jsx
@@ -1,6 +1,13 @@
+import styles from "./Item.module.css";
+
 const Item = (props) => {
   let { foodItem } = props; // array-destructuring
-  return <li className="list-group-item">{foodItem}</li>;
+  return (
+    <li className={`${styles["food-list"]} list-group-item`}>
+      <span className={styles["food-item"]}>{foodItem}</span>
+      {/* Css modules - for component level styling, generates special hash at the end of class to differentiate b/w different components styling of same type */}
+    </li>
+  );
 };
 
 export default Item;
