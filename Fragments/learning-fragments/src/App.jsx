ac5832c15d2ce4d0ef600e5aba8355d764969017
uttam8299 <uttam.yadav8299@gmail.com>
2024-03-05 00:26:47 +0530

    map method

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -6,15 +6,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
 function App() {
+  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+
   return (
     <>
       <h1>Healthy Food</h1>
-      <ul class="list-group">
-        <li class="list-group-item">Dal</li>
-        <li class="list-group-item">Green Vegetables</li>
-        <li class="list-group-item">Roti</li>
-        <li class="list-group-item">Salad</li>
-        <li class="list-group-item">Milk</li>
+      <ul className="list-group">
+        {foodItems.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
