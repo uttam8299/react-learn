388d4aac61cab598fe9d57b32b2422eddfa43842
uttam8299 <uttam.yadav8299@gmail.com>
2024-03-06 01:03:40 +0530

    passing children in Container

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
   let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
   return (
     <>
-      <h1>Healthy Food</h1>
-      <ErrorMessage foodItems={foodItems}></ErrorMessage>
-      <FoodItems foodItems={foodItems}></FoodItems>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <ErrorMessage foodItems={foodItems}></ErrorMessage>
+        <FoodItems foodItems={foodItems}></FoodItems>
+      </Container>
+
+      <Container>
+        <p>
+          Above are list of healthy items for you to keep healthy and well
+          being.
+        </p>
+      </Container>
     </>
   );
 }
