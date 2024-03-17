76d916f350232d038c231b4ccc95bb7c41484ec8
uttam8299 <uttam.yadav8299@gmail.com>
2024-03-18 00:04:44 +0530

    hooks - using useState()

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import React, { useState } from "react";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import Container from "./components/Container";
@@ -9,8 +9,10 @@ import FoodInput from "./components/FoodInput";
 function App() {
   let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
 
+  let [textStateVal, setTextState] = useState("User input will appear here");
+
   const handleChangeEvent = (event) => {
-    console.log(event.target.value);
+    setTextState(event.target.value);
   };
 
   return (
@@ -19,6 +21,7 @@ function App() {
         <h1 className="food-heading">Healthy Food</h1>
         <ErrorMessage foodItems={foodItems}></ErrorMessage>
         <FoodInput handleChangeEvent={handleChangeEvent}></FoodInput>
+        <p>{textStateVal}</p>
         <FoodItems foodItems={foodItems}></FoodItems>
       </Container>
     </>
