b44b30bc488b14ba1bd2d95dab1f3a1c9fb65e51
uttam8299 <uttam.yadav8299@gmail.com>
2024-03-30 00:39:15 +0530

    Hooks - useEffect

--- Advanced-react/social-media/src/components/PostList.jsx
+++ Advanced-react/social-media/src/components/PostList.jsx
@@ -2,23 +2,22 @@ import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import { useContext } from "react";
 import WelcomeMessage from "./WelcomeMessage";
+import { useEffect } from "react";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
 
-  const handleGetPostsClick = () => {
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
+  }, []); // second arg is dependency list. this hooks runs when variables in dependecy list changes
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClicked={handleGetPostsClick} />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- Advanced-react/social-media/src/components/WelcomeMessage.jsx
+++ Advanced-react/social-media/src/components/WelcomeMessage.jsx
@@ -1,14 +1,7 @@
-const WelcomeMessage = ({ onGetPostsClicked }) => {
+const WelcomeMessage = () => {
   return (
     <center className="welcome-message">
-      <h1>There are no posts.</h1>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={onGetPostsClicked}
-      >
-        Get Posts from Server
-      </button>
+      <h1>There are no posts.</h1>{" "}
     </center>
   );
 };
